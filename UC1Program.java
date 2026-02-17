f04b894cbe12b620dd3ba3e751694283b90eca7d
ak0289-tech <ak0289@srmist.edu.in>
2026-02-17 11:36:24 +0530

    this is my UC 3 code

--- UC1Program.java
+++ UC1Program.java
@@ -1,5 +1,5 @@
 public class UC1Program {
     public static void main(String[] args) {
-        System.out.println("Welcome to User Registration Program");
+        System.out.println("OOPS");
     }
 }
